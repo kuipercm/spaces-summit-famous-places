2a1e67cd5c41ddc669f5d49dccfbfe8655580ade
Niels Kuiper <kuiper.cm@gmail.com>
2021-04-11 16:30:23 +0200

    Update terraform - remove project definition - should exist

--- main.tf
+++ main.tf
@@ -19,13 +19,6 @@ provider "google-beta" {
   credentials = var.google_credentials
 }
 
-resource "google_project" "spaces_summit_famous_places" {
-  name            = "Spaces Summit Famous Places"
-  project_id      = var.projectname
-  folder_id       = var.folder_id
-  billing_account = var.billing_account
-}
-
 resource "google_cloudbuild_trigger" "famous_places_build_trigger" {
   provider = "google"
   description = "Build famous places repo from github when push to master detected"
